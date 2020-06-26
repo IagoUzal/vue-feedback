bef80ccb68de421b0191d00e93d7e882253d2177
iago <iagouzal@gmail.com>
2020-06-26 13:53:40 +0200

    feedback enviado, get, put, delete

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -2,7 +2,6 @@
   <main class="profile container">
     <section class="profile">
       <h3>Tus datos de perfil</h3>
-      <p>Cambia aquí tus datos para actualizarlos</p>
       <img class="avatar" :src="profile.avatar" :alt="profile.name" />
       <input
         type="file"
@@ -21,10 +20,34 @@
       <br />
       <button class="button_primary" @click="editProfile()">Actualizar</button>
     </section>
+    <section class="feedback">
+      <h3>Feedback enviado</h3>
+      <ul>
+        <li v-for="feedback in feedbackSend" :key="feedback.id">
+          <img class="avatar" :src="feedback.avatar_to" :alt="feedback.Para" />
+          <p>{{ feedback.Para }}</p>
+          <h3>{{ feedback.title }}</h3>
+          <p>{{ feedback.text }}</p>
+          <img :src="feedback.image" :alt="feedback.title" />
+          <p>{{ feedback.category }}</p>
+          <p>{{ feedback.type }}</p>
+        </li>
+      </ul>
+    </section>
   </main>
 </template>
 
 <script>
+/*
+  - FIXME: 
+    - actualizar avatar
+    - actualizar password 
+
+  - TODO: 
+    - edit messages escritos
+    - delete messages escritos
+    - maquetación
+*/
 import axios from "axios";
 
 export default {
@@ -32,6 +55,7 @@ export default {
   data() {
     return {
       profile: [],
+      feedbackSend: [],
     };
   },
   methods: {
@@ -50,19 +74,37 @@ export default {
           }
         });
     },
-
+    // editar datos de usuario
     editProfile() {
       let self = this;
+      let userID = this.$route.params.id;
 
       axios
-        .put("http://localhost:3001/users/" + self.$route.params.id, {
+        .put("http://localhost:3001/users/" + userID, {
+          avatar: this.profile.avatar,
           name: this.profile.name,
           surname: this.profile.surname,
-          email: this.profile.surname,
+          email: this.profile.email,
           location: this.profile.location,
         })
         .then(function(response) {
-          alert("Usuario modificado");
+          alert("Usuario modificado correctamente");
+        })
+        .catch(function(error) {
+          if (error.response) {
+            alert(error.response.data.message);
+          }
+        });
+    },
+    getFeedbackSend() {
+      let self = this;
+      let userID = this.$route.params.id;
+
+      axios
+        .get(`http://localhost:3001/messages/users/${userID}`)
+        .then(function(response) {
+          self.feedbackSend = response.data.data;
+          console.log(self.feedbackSend);
         })
         .catch(function(error) {
           if (error.response) {
@@ -73,6 +115,7 @@ export default {
   },
   created() {
     this.getProfile();
+    this.getFeedbackSend();
   },
 };
 </script>
