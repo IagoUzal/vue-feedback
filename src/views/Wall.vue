51215051a00afe5862a905ae20c57cf0dc17492c
iago <iagouzal@gmail.com>
2020-06-29 19:01:16 +0200

    page wall

--- src/views/Wall.vue
+++ src/views/Wall.vue
@@ -1,5 +1,103 @@
 <template>
-  <main></main>
+  <main class="wall">
+    <article class="container">
+      <section class="perfil">
+        <section class="user_info">
+          <h2>Perfil de {{ user.name }} 🤗</h2>
+          <img class="avatar" :src="user.avatar" :alt="user.name" />
+          <ul>
+            <li>Nombre: {{ user.name }}</li>
+            <li>Apellidos: {{ user.surname }}</li>
+            <li>Localidad: {{ user.location }}</li>
+          </ul>
+        </section>
+      </section>
+      <section class="wall_feedback">
+        <section class="send_feedback">
+          <h2>Enviar feedback a {{ user.name }} 💬</h2>
+          <div class="form">
+            <label for="title">Titulo</label>
+            <br />
+            <input
+              type="text"
+              id="Titulo"
+              v-model="title"
+              placeholder="Titulo corto y claro"
+            />
+            <br />
+            <label for="text">Descripción</label>
+            <br />
+            <textarea
+              name="text"
+              id="text"
+              cols="30"
+              rows="4"
+              v-model="text"
+            ></textarea>
+            <br />
+            <label for="category">Categoria</label>
+            <br />
+            <select name="category" v-model="category">
+              <option value="Profesional" selected>Profesional</option>
+              <option value="Personal">Personal</option>
+            </select>
+            <br />
+            <label for="type">Tipo</label>
+            <br />
+            <select name="type" id="type" v-model="type">
+              <option value="Agradecimiento">Agradecimiento</option>
+              <option value="Referencia" selected>Referencia</option>
+            </select>
+            <br />
+            <input type="file" id="file" ref="file" @change="onFileChanged" />
+            <br />
+            <button class="button_primary" @click="sendFeedback()">
+              Enviar feedback
+            </button>
+          </div>
+        </section>
+        <section class="feedback_recibido">
+          <h2>Último feedback recibido por {{ user.name }} 👇</h2>
+          <ul>
+            <li v-for="message in messages" :key="message.id">
+              <div class="container_card">
+                <div class="card_header">
+                  <h3>{{ message.title }}</h3>
+                  <p>Escrito por: {{ message.De }}</p>
+                </div>
+                <div class="card_body">
+                  <p>{{ message.text }}</p>
+                  <img
+                    :src="
+                      message.image === 'sin imagen'
+                        ? message.image
+                        : path + message.image
+                    "
+                    :alt="message.titulo"
+                    :class="{ sinImagen: message.image === 'sin imagen' }"
+                    class="img_feedback"
+                  />
+                </div>
+                <div class="card_footer">
+                  <p>{{ message.category }}</p>
+                  <p>{{ message.type }}</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </section>
+      <section class="others">
+        <h2>Otros</h2>
+        <section class="others_wrapper">
+          <p class="label">
+            {{ totalFeedback }} <br />
+            Feedback recibido
+          </p>
+        </section>
+      </section>
+    </article>
+  </main>
 </template>
 
 <script>
@@ -7,7 +105,144 @@ import axios from "axios";
 
 export default {
   name: "Wall",
+  components: {},
+  data() {
+    return {
+      messages: [],
+      user: [],
+      path: "http://localhost:3001/uploads/",
+      title: "",
+      text: "",
+      category: "",
+      type: "",
+      file: null,
+      fileNo: "sin imagen",
+      totalFeedback: 0,
+    };
+  },
+  methods: {
+    onFileChanged() {
+      this.file = this.$refs.file.files[0];
+    },
+    getMessagesPara() {
+      let self = this;
+      let para = this.$route.params.id;
+
+      axios
+        .get(`http://localhost:3001/messages/to/${para}`)
+        .then(function(response) {
+          self.messages = response.data.data;
+          console.log(self.messages);
+          self.totalFeedback = self.messages.length;
+        })
+        .catch(function(error) {
+          if (error.response) {
+            alert(error.response.data.message);
+          }
+        });
+    },
+    getUserInfo() {
+      let self = this;
+      let userID = this.$route.params.id;
+
+      axios
+        .get(`http://localhost:3001/users/${userID}`)
+        .then(function(response) {
+          self.user = response.data.data[0];
+          console.log(self.user);
+        });
+    },
+    sendFeedback() {
+      let self = this;
+      let toUserID = this.$route.params.id;
+
+      let dataNewFeedback = new FormData();
+      dataNewFeedback.append("to_users_id", toUserID);
+      dataNewFeedback.append("title", this.title);
+      dataNewFeedback.append("text", this.text);
+      dataNewFeedback.append("category", this.category);
+      dataNewFeedback.append("type", this.type);
+      if (this.file !== null) {
+        dataNewFeedback.append("image", this.file);
+      } else {
+        dataNewFeedback.append("image", this.fileNo);
+      }
+
+      axios
+        .post("http://localhost:3001/messages", dataNewFeedback)
+        .then(function(response) {
+          alert("Feedback enviado correctamente");
+          setTimeout(function() {
+            location.reload();
+          }, 1500);
+        })
+        .catch(function(error) {
+          if (error.response) {
+            alert(error.response.data.message);
+          }
+        });
+    },
+  },
+  created() {
+    this.getMessagesPara();
+    this.getUserInfo();
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  gap: 2rem;
+}
+
+.wall {
+  margin: 2rem 0;
+}
+h2 {
+  margin-bottom: 2rem;
+}
+
+.perfil {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 1rem;
+}
+
+.sinImagen {
+  display: none;
+}
+
+.label {
+  background: #162447;
+  color: #fff;
+}
+
+.user_info li {
+  border-bottom: 1px solid #505050;
+  padding: 0.5rem 0;
+  width: 100%;
+}
+
+.form {
+  background: #fff;
+  padding: 2rem;
+  border-radius: 6px;
+}
+
+.feedback_recibido ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 1rem;
+}
+
+.feedback_recibido ul li {
+  background: #fff;
+  padding: 2rem;
+}
+
+.img_feedback {
+  width: 100%;
+}
+</style>
