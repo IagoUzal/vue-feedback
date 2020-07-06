37d0c8e149af25d4e142e47238a167ba18380eaf
iago <iagouzal@gmail.com>
2020-07-06 15:44:36 +0200

    404

---
+++ src/views/Error.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="container">
+    <vue-headful title="404 | Feedback" description="No hay nada aquí" />
+    <h1>Error</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Error",
+};
+</script>
+
+<style></style>
